3d93d5d665457a278c9182cffc1f8819a420f646
Kaj <kibunjiajulius@gmail.com>
2019-09-10 08:00:35 +0300

    Change output formatting to use pygments.highlight

--- irepl.py
+++ irepl.py
@@ -3,9 +3,10 @@
 import re
 import configparser
 import pygments
-from pygments.styles import get_style_by_name
-from prompt_toolkit.styles.pygments import style_from_pygments_cls
 from pygments.lexers import find_lexer_class_by_name
+from pygments.styles import get_style_by_name
+from pygments.formatters import TerminalTrueColorFormatter
+from prompt_toolkit.styles.pygments import style_from_pygments_cls
 from prompt_toolkit import PromptSession, print_formatted_text
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.lexers import PygmentsLexer
@@ -26,8 +27,9 @@ def build_completion_list(lexer):
         completions.extend(list(lexer.keywords))
     return completions
 
-yaml = YAML(typ='safe')
-f = open('langs.yml', 'r')
+
+yaml = YAML(typ="safe")
+f = open("langs.yml", "r")
 CONFIG = yaml.load(f)
 f.close()
 
@@ -36,16 +38,20 @@ try:
 except IndexError:
     LANGUAGE = "factor"
 
-PROMPT = CONFIG[LANGUAGE.upper()]['prompt']
-LEXER = find_lexer_class_by_name(CONFIG[LANGUAGE.upper()]['lexer'])
-EXE = CONFIG[LANGUAGE.upper()]['executable']
+PROMPT = CONFIG[LANGUAGE.upper()]["prompt"]
+LEXER_CLS = find_lexer_class_by_name(CONFIG[LANGUAGE.upper()]["lexer"])
+EXE = CONFIG[LANGUAGE.upper()]["executable"]
 
-STYLE = style_from_pygments_cls(get_style_by_name("monokai"))
+STYLE = get_style_by_name("monokai")
+LEXER_INS = LEXER_CLS()
 
-COMPLETER = WordCompleter(build_completion_list(LEXER))
+COMPLETER = WordCompleter(build_completion_list(LEXER_CLS))
 
 sess = PromptSession(
-    f"({LANGUAGE})> ", style=STYLE, lexer=PygmentsLexer(LEXER), completer=COMPLETER
+    f"({LANGUAGE})> ",
+    style=style_from_pygments_cls(STYLE),
+    lexer=PygmentsLexer(LEXER_CLS),
+    completer=COMPLETER,
 )
 #  https://stackoverflow.com/questions/14693701/how-can-i-remove-the-ansi-escape-sequences-from-a-string-in-python
 ansi_escape = re.compile(
@@ -73,13 +79,11 @@ while True:
             output = re.sub(ansi_escape, "", repl.before)[1:]
         else:
             output = repl.before
-        #  print(output, end="")
-        tokens = list(pygments.lex(output, lexer=LEXER()))
-        formatted_output = StringIO()
-        print_formatted_text(PygmentsTokens(tokens), style=STYLE, file=formatted_output)
-        print(formatted_output.getvalue())
+        formatted_output = pygments.highlight(
+            output, LEXER_INS, TerminalTrueColorFormatter(style=STYLE)
+        )
+        print(formatted_output)
     except KeyboardInterrupt:
         print("User interrupt")
         repl.close()
         break
-#  prev = ''
