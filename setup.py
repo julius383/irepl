ae08177bb25d431adbc3ccd616eb60b917770477
Kaj <kibunjiajulius@gmail.com>
2019-09-16 14:29:39 +0300

    Add package specific files

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(name='irepl',
+      version='0.1',
+      description='Wrapper to enhance any repl',
+      author='Julius Kibunjia',
+      author_email='kibunjiajulius@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      entry_points={
+    'console_scripts': [
+        'irepl = irepl.irepl:main',
+    ],
+},
+                 )
