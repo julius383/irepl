c91a179dc7edff6821766421b600ae1f75d088f1
Kaj <kibunjiajulius@gmail.com>
2019-12-11 21:41:40 +0300

    Rewrite to use pseudo terminals instead of pexpect library

--- irepl/irepl.py
+++ irepl/irepl.py
@@ -2,10 +2,18 @@
 
 import re
 import tempfile
-import subprocess
 import shutil
 import sys
+import os
 from os import path
+import subprocess
+import pty
+from select import select
+import tty
+import errno
+import shlex
+from itertools import repeat
+
 import pygments
 from pygments.lexers import find_lexer_class_by_name
 from pygments.styles import get_style_by_name
@@ -21,6 +29,26 @@ from prompt_toolkit.formatted_text import PygmentsTokens
 import pexpect
 from ruamel.yaml import YAML
 
+#  from keys import BINDINGS, MULTILINE
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.application import run_in_terminal
+from prompt_toolkit.filters import Condition
+
+BINDINGS = KeyBindings()
+MULTILINE = False
+
+
+@BINDINGS.add("escape", "enter")
+def _(event):
+    global MULTILINE
+    MULTILINE = not MULTILINE
+
+
+@BINDINGS.add("c-x")
+def _(event):
+    " Exit when `c-x` is pressed. "
+    event.app.exit()
+
 
 STYLE_DICT = {Token.Prompt: "#85678f", Token.Info: "#b294bb bold"}
 
@@ -34,127 +62,110 @@ STYLE = merge_styles(
 #  https://stackoverflow.com/questions/14693701/how-can-i-remove-the-ansi-escape-sequences-from-a-string-in-python
 ANSI_ESCAPE = re.compile(
     r"""
-    \x1B    # ESC
+\x1B    # ESC
     [@-_]   # 7-bit C1 Fe
     [0-?]*  # Parameter bytes
     [ -/]*  # Intermediate bytes
     [@-~]   # Final byte
-""",
+    """,
     re.VERBOSE,
 )
 
-
-class ReplWrap(object):
-    def __init__(self, rname):
-        self.rname = rname.upper()
-        self.pkg_dir = path.dirname(__file__)
-        self.lang_params = self.load_language_config()
-        self.lexer_cls = find_lexer_class_by_name(self.lang_params["lexer"])
-        self.prompt = self.lang_params["prompt"]
-        self.exe = self.lang_params["executable"]
-        self.completer = WordCompleter(self.build_completion_list())
-        self.prompt_sess = PromptSession(
-            style=STYLE,
-            lexer=PygmentsLexer(self.lexer_cls),
-            completer=self.completer,
-            include_default_pygments_style=False,
-            enable_open_in_editor=True,
-        )
-        self.repl = self.init_repl()
-        self.pager_file = open(tempfile.mkstemp()[1], "w")
-        self.lexer_ins = self.lexer_cls()
-
-    def load_language_config(self):
-        yaml = YAML(typ="safe")
-        f = open(path.join(self.pkg_dir, "langs.yml"), "r")
-        config = yaml.load(f)
-        f.close()
-        return config[self.rname]
-
-    def build_completion_list(self):
-        completions = []
-        if hasattr(self.lexer_cls, "keywords"):
-            completions.extend(list(self.lexer_cls.keywords))
-        if hasattr(self.lexer_cls, "builtins"):
-            completions.extend(list(self.lexer_cls.builtins))
-        if hasattr(self.lexer_cls, "keywords"):
-            completions.extend(list(self.lexer_cls.keywords))
-        return completions
-
-    def init_repl(self):
-        repl = pexpect.spawn(self.exe, encoding="utf-8")
-        repl.setecho(False)
-        repl.waitnoecho()
-        repl.expect(self.prompt)
-        return repl
-
-    def create_prompt(self):
-        if self.repl.match.groups():
-            info = " ".join([f"({m})" for m in self.repl.match.groups()])
-            text = PygmentsTokens(
-                [
-                    (Token.Prompt, f"({self.rname}):"),
-                    (Token.Info, info),
-                    (Token.Text, " > "),
-                ]
-            )
-        else:
-            text = PygmentsTokens(
-                [(Token.Prompt, f"({self.rname}):"), (Token.Text, " > ")]
-            )
-        return text
-
-    def get_eval_results(self):
-        user_in = self.prompt_sess.prompt(self.create_prompt())
-        self.repl.sendline(user_in)
-        self.repl.expect(self.prompt)
-        if re.search(ANSI_ESCAPE, self.repl.before):
-            output = re.sub(ANSI_ESCAPE, "", self.repl.before)[1:]
-        else:
-            output = self.repl.before
-        return output
-
-    def process_eval_results(self, output):
-        page = False
-        if (
-            len(re.findall(r"\n", output))
-            > shutil.get_terminal_size().lines - 1
-        ):
-            page = True
-
-        formatted_output = pygments.highlight(
-            output,
-            self.lexer_ins,
-            TerminalTrueColorFormatter(style=get_style_by_name("monokai")),
-        )
-        #  https://stackoverflow.com/questions/6728661/paging-output-from-python
-        if page:
-            print(formatted_output, file=self.pager_file)
-            self.pager_file.flush()
-            subprocess.call(["less", "-R", self.pager_file.name])
-        else:
-            print(formatted_output)
-
-    def cleanup(self):
-        self.pager_file.close()
-        self.repl.close()
+LANGUAGE = "PYTHON"
 
 
-def main():
-    try:
-        LANGUAGE = sys.argv[1]
-    except IndexError:
-        LANGUAGE = "factor"
-    wrapped = ReplWrap(LANGUAGE)
-    while True:
-        try:
-            repl_output = wrapped.get_eval_results()
-            wrapped.process_eval_results(repl_output)
-        except KeyboardInterrupt:
-            print("User interrupt")
-            wrapped.cleanup()
-            break
+def load_config():
+    yaml = YAML(typ="safe")
+    config_file = path.join(path.dirname(__file__), "langs.yml")
+    with open(config_file, "r") as f:
+        config_dict = yaml.load(f)
+    return config_dict
 
 
-if __name__ == "__main__":
-    main()
+def build_completion_list(lexer):
+    completions = []
+    if hasattr(lexer, "keywords"):
+        completions.extend(list(lexer.keywords))
+    if hasattr(lexer, "builtins"):
+        completions.extend(list(lexer.builtins))
+    if hasattr(lexer, "keywords"):
+        completions.extend(list(lexer.keywords))
+    return completions
+
+
+CONFIG = load_config()
+LANG_CONFIG = CONFIG[LANGUAGE]
+LEXER = find_lexer_class_by_name(LANG_CONFIG["lexer"])
+COMPLETER = WordCompleter(build_completion_list(LEXER))
+PAGER_FILE = open(tempfile.mkstemp()[1], "w")
+LEXER_INS = LEXER()
+
+PROMPT = PromptSession(
+    style=STYLE,
+    lexer=PygmentsLexer(LEXER),
+    completer=COMPLETER,
+    include_default_pygments_style=False,
+    enable_open_in_editor=True,
+    key_bindings=BINDINGS,
+)
+
+
+def format_output(output):
+    page = False
+    if len(re.findall(r"\n", output)) > shutil.get_terminal_size().lines - 1:
+        page = True
+
+    formatted_output = pygments.highlight(
+        output,
+        LEXER_INS,
+        TerminalTrueColorFormatter(style=get_style_by_name("monokai")),
+    )
+    #  https://stackoverflow.com/questions/6728661/paging-output-from-python
+    if page:
+        print(formatted_output, file=PAGER_FILE)
+        PAGER_FILE.flush()
+        subprocess.call(["less", "-R", PAGER_FILE.name])
+    else:
+        print(formatted_output)
+
+
+mo, so = pty.openpty()
+me, se = pty.openpty()
+mi, si = pty.openpty()
+
+p = subprocess.Popen(
+    shlex.split(LANG_CONFIG["executable"]),
+    bufsize=1,
+    stdout=so,
+    stdin=si,
+    stderr=se,
+    close_fds=True,
+)
+
+
+def read_from(fds):
+    ready, _, _ = select(fds, [], [], 0.04)
+    result = dict(zip(fds, repeat(b'')))
+    if ready:
+        for fd in ready:
+            data = os.read(fd, 512)
+            if not data:
+                break
+            result[fd] += data
+    return result
+
+
+while True:
+    result = read_from([mo, me])
+    result = read_from([mo, me])
+    if p.poll() is not None:  # select timed-out
+        break
+    if (out := result[mo]):
+        print(f"{out=}")
+        #  format_output(str(out))
+    if (err := result[me]):
+        print(f"{err=}")
+        #  print(str(err), file=sys.stderr)
+    user_in = PROMPT.prompt("> ")
+    os.write(mi, str.encode(user_in + "\r\n"))
+    print(f"{user_in=}")
