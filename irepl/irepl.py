871b0023b2c2446cf3e4735e4023f603a98a7220
Kaj <kibunjiajulius@gmail.com>
2020-01-02 21:22:13 +0300

    Add zeromq based remote evaluation

--- irepl/irepl.py
+++ irepl/irepl.py
@@ -1,20 +1,26 @@
 
-from interactive import InteractiveMixin
-from config import load_config_for
-from wrappedrepl import WrappedRepl
 import os
 import signal
+from interactive import InteractiveMixin
+from client import RemoteMixin
+from config import load_config_for
+from wrappedrepl import WrappedRepl
 
 
 class IRepl(InteractiveMixin, WrappedRepl):
     pass
 
 
+class RRepl(RemoteMixin, WrappedRepl):
+    pass
+
+
 if __name__ == "__main__":
-    config = load_config_for("haskell")
+    c = load_config_for("haskell")
     try:
         os.setpgrp()
-        r = IRepl(config)
+        r = RRepl(config=c)
+        #  r.get_input()
         r.run()
-    finally:
+    except KeyboardInterrupt:
         os.killpg(os.getpgrp(), signal.SIGTERM)
--- irepl/remote.py
+++ irepl/remote.py
@@ -1,14 +1,37 @@
-#!/usr/bin/env python3
-import os
-import time
 import zmq
 
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
 
-socket.connect("tcp://localhost:5556")
-socket.setsockopt_string(zmq.SUBSCRIBE, "")
-data = socket.recv_string()
-while data:
-    print("{0} = {1}".format(data, eval(data)))
-    data = socket.recv_string()
+def lcg(mod, a, c, seed):
+    """Linear congruential generator."""
+    while True:
+        seed = (a * seed + c) % mod
+        yield seed
+
+#  open_port = lcg(mod=65536, a=33219, c=1, seed=5000)
+
+
+class RemoteMixin(object):
+    def __init__(self, **kwargs):
+        super(RemoteMixin, self).__init__(**kwargs)
+        self.config = kwargs['config']
+        self.initialize_listener(
+            self.config['lang'],
+            kwargs.get('host', "tcp://localhost"),
+            kwargs.get('port', 5536)
+        )
+
+    def initialize_listener(self, lang, host, port):
+        context = zmq.Context()
+        socket = context.socket(zmq.SUB)
+        socket.setsockopt(zmq.LINGER, 0)
+        socket.setsockopt_string(zmq.SUBSCRIBE, lang)
+        socket.connect(f"{host}:{port}")
+        self.socket = socket
+        self.port = port
+        self.host = host
+
+    def get_input(self):
+        print(f"\n--- waiting for input on {self.host}:{self.port}---")
+        s = self.socket.recv_string()
+        print(s)
+        return s
